5347c388c557a4616371752c3ab22be514d6686f
PyrexBLJ <49227284+PyrexBLJ@users.noreply.github.com>
2024-11-10 21:02:58 -0500

    pearl detector
    prints a bog ass bright blue message on screen when a pearl spawns in

--- BonkUtilities/__init__.py
+++ BonkUtilities/__init__.py
@@ -22,6 +22,7 @@ MsgDisplayTime: SliderOption = SliderOption("Top Screen Message Time", 3.5, 0, 5
 UseHLQNoclip: BoolOption = BoolOption("Use HLQ Noclip", True, "Yes", "No")
 DesiredFPS: SliderOption = SliderOption("Desired FPS", 120, 30, 1024, 1, True, on_change = lambda _, new_value: setFPS(_, new_value))
 MapforTravel: DropdownOption = DropdownOption("Map for Travel Keybind", "arid_p", maps.maplist)
+PearlDetector: BoolOption = BoolOption("Pearl Item Detector", True, "On", "Off")
 
 
 class location(TypedDict):
@@ -298,6 +299,18 @@ def exitPhasewalk(obj: UObject, __args: WrappedStruct, __ret: any, __func: Bound
     if wantstophasejump == True:
         unrealsdk.find_all("WillowPlayerInput")[-1].Jump()
         wantstophasejump = False
+    return None
+# 101-169 pearl rarity
+@hook(hook_func="WillowGame.WillowPickup:InitializeFromInventory", hook_type=Type.POST)
+def detectPearl(obj: UObject, __args: WrappedStruct, __ret: any, __func: BoundFunction) -> None:
+    if obj.InventoryRarityLevel > 100 and obj.InventoryRarityLevel < 170 and PearlDetector.value == True:
+        get_pc().myHUD.GetHUDMovie().AddCriticalText(0, "<font color = \"#00ffc8\" size = \"32\">Pearl Drop Detected!</font>", 5.0, get_pc().myHUD.WhiteColor, get_pc().myHUD.WPRI)
+    return None
+
+@hook(hook_func="WillowGame.WillowPickup:AdjustPickupPhysicsAndCollisionForBeingAttached", hook_type=Type.POST)
+def detectPearlChest(obj: UObject, __args: WrappedStruct, __ret: any, __func: BoundFunction) -> None:
+    if obj.InventoryRarityLevel > 100 and obj.InventoryRarityLevel < 170 and PearlDetector.value == True:
+        get_pc().myHUD.GetHUDMovie().AddCriticalText(0, "<font color = \"#00ffc8\" size = \"32\">Pearl Drop Detected!</font>", 5.0, get_pc().myHUD.WhiteColor, get_pc().myHUD.WPRI)
     return None
 
 build_mod()
